82d9fcd4ac2f3cf738e018a4b7e1049ebbb8aa3d
Jenqyang <jenqyang@qq.com>
2025-01-29 17:01:38 +0800

    chore: update author information and improve README clarity

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  title: 'Zhengyang Hou',
+  author: 'Zhengyang Hou',
+  email: 'jenqyanghou@gmail.com',
+  link: 'https://www.zhengyang.me/',
+  description: 'Bringing beautiful visual and software experiences to people.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
@@ -18,9 +18,9 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://x.com/Jenqyanghou',
   seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
+    keywords: ['Zhengyang', 'Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
@@ -59,4 +59,4 @@ const BLOG = {
   isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
 }
 // export default BLOG
-module.exports = BLOG
+export default BLOG
